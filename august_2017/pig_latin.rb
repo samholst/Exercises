72e3b21c1e65664e2c10c3cc62bf500609de1291
Sam Holst <sawohol@gmail.com>
2017-08-29 10:10:48 -0600

    updated piglatin

--- august_2017/pig_latin.rb
+++ august_2017/pig_latin.rb
@@ -1,27 +1,41 @@
 require 'rspec'
-
+require 'byebug'
 # Write a method that translates a text to Pig Latin and back.
 # English is translated to Pig Latin by taking the first letter of every word,
 # moving it to the end of the word and adding 'ay'.
 # The quick brown fox becomes "Hetay uickqay rownbay oxfay"
 
 def transform_pig_latin sentence
-  temp = sentence.downcase.split
+  temp = sentence.downcase.split(/ /)
   new_array = []
 
   temp.each do |word|
-    word += word[0] << "ay"
-    new_array << word.slice(1, word.length - 1)
+
+    if word[0] =~ /[aeiou]/
+      word += "ay"
+      new_array << word
+    else
+      word += word[0] << "ay"
+      new_array << word.slice(1, word.length - 1)
+    end
   end
+
   new_array.join(' ').capitalize
 end
 
-sentence = 'The quick brown fox'
+
+
+
+sentence = 'The quick brown is a fox'
 p transform_pig_latin(sentence)
+
+
+
+
 
 describe 'PigLatin' do
   it 'takes the first letter of every word and moves it to the end of the word and adds ay' do
-    sentence = 'The quick brown fox'
-    expect(transform_pig_latin(sentence)).to eq('Hetay uickqay rownbay oxfay')
+    sentence = 'The quick brown is a fox'
+    expect(transform_pig_latin(sentence)).to eq('Hetay uickqay rownbay isay aay oxfay')
   end
 end
