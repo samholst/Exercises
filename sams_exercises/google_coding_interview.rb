fdf3080050f85443710ef84846d92c02b6409d9d
Sam Holst <sawohol@gmail.com>
2017-12-04 15:00:55 -0700

    updated test

--- sams_exercises/google_coding_interview.rb
+++ sams_exercises/google_coding_interview.rb
@@ -1,3 +1,5 @@
+require 'rspec'
+
 arr = [1,2,3,9]
 arr_2 = [1,1,11,334,33,22,2,4,4,15,44,33,10]
 
@@ -13,5 +15,23 @@ def find_sum(num_set, sum)
   return false
 end
 
-find_sum(arr_2, 25)
+find_sum(arr_2, 27)
 # find_sum(arr, 25)
+
+
+describe 'Find Sum In a Set of Numbers' do
+  it 'finds the sum of 25' do
+    arr = [1,1,11,334,33,22,2,4,4,15,44,33,10]
+    expect(find_sum arr, 25).to eq(true)
+  end
+
+  it 'finds the sum 8' do
+    arr = [1,1,11,334,33,22,2,4,4,15,44,33,10]
+    expect(find_sum arr, 8).to eq(true)
+  end
+
+  it 'does not find the sum 16' do
+    arr = [1,1,11,334,33,22,2,4,4,15,44,33,10]
+    expect(find_sum arr, 27).to eq(false)
+  end
+end
